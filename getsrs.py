e42759d6d48bff9f233cfc4eeb4b830d8130e26c
Chris Xiong <chrisxiongyang@yahoo.com>
2021-04-20 16:39:43 -0700

    add framework for sr similarity

---
+++ getsrs.py
@@ -0,0 +1,33 @@
+def get_sr(map):
+    # TODO: get sr of map file
+    return (0, 0)
+
+def get_srs(srs_file='srs.txt'):
+    srs = {}
+    try:
+        with open(srs_file, 'r') as f:
+            lines = f.readlines()
+        for i in range(0, len(lines), 2):
+            srs[lines[i].strip()] = tuple(float(x) for x in lines[i+1].split(','))
+    except:
+        pass
+
+    return srs
+
+if __name__ == '__main__':
+    raw = 'srs_raw.txt'
+    start_line = 5
+    end_line = 31613
+    line_step = 2
+
+    with open(raw, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+
+    with open('srs.txt', 'w', encoding='utf8') as f:
+        for i in range(start_line, end_line, line_step):
+            line = lines[i]
+            song = line[:160]
+            song = song[song.index('-')+1:].strip()
+            sr, aim, speed, combo, ar, od = (float(x) for x in line[161:].strip('\n║').split('│'))
+
+            f.write(f'{song}\n{aim},{speed}\n')
---
+++ similarity_srs.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+
+import getmaps
+import getsrs
+
+def manhattan(a, b):
+    return sum(abs(a[i] - b[i]) for i in range(len(a)))
+
+def get_similar(id, n=50):
+    chars = '1234567890qwertyuiopasdfghjklzxcvbnm'
+    temp_filename = ''.join(chars[random.randrange(len(chars))] for _ in range(10)) + '.osu'
+
+    filename, text = getmaps.get_map(id)
+    with open(temp_filename, 'w', encoding='utf8', newline='') as f:
+        f.write(text)
+    sr = getsrs.get_sr(temp_filename)
+
+    os.remove(temp_filename)
+
+    similarities = []
+
+    for file in srs:
+        if filename.startswith(file):
+            continue
+
+        similarities.append((file, manhattan(sr, srs[file])))
+
+    similarities.sort(key=lambda s: -s[1])
+    return similarities[:n]
+
+srs = getsrs.get_srs()
+
+if __name__ == '__main__':
+    get_similar(2659369)
