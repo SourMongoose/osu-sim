853f9cd62b82cb0a71f3974f1812adf9bb916d22
Chris Xiong <chrisxiongyang@yahoo.com>
2021-04-27 13:44:40 -0700

    maps update 4/27/21

--- getmaps.py
+++ getmaps.py
@@ -9,30 +9,35 @@ def get_map(id):
     return filename, r.text
 
 if __name__ == '__main__':
-    mapids_file = r'C:\Users\chris\Documents\Python Workspace\osurec\mapids_nodup.txt'
+    mapids_file = r'C:\Users\chris\Documents\Python Workspace\osurec\mapids_country_nodup.txt'
     maps_folder = 'maps'
 
     with open(mapids_file, 'r') as f:
         mapids = f.readlines()
 
-    files = [entry.name for entry in os.scandir(maps_folder)]
+    with open('filenames.txt', 'r') as f:
+        files = f.readlines()
+    files = set(files[i] for i in range(0, len(files), 2))
 
-    idx = 5170
+    idx = 0
     while idx < len(mapids):
         print(idx)
+
+        if mapids[idx] in files:
+            idx += 1
+            continue
+
         try:
             filename, text = get_map(mapids[idx].strip())
         except:
             continue
 
+        with open('filenames.txt', 'a', encoding='utf8') as f:
+            f.write(mapids[idx] + filename + '\n')
+
         #if not os.path.exists(path):
         if filename not in files:
             with open(os.path.join(maps_folder, filename), 'w', encoding='utf8', newline='') as f:
                 f.write(text)
-        else:
-            idx += 1
-            continue
 
         idx += 1
-
-        sleep(1)
